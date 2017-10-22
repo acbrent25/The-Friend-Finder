93f4333ff25f4f2eb5e429716e1b05ad7ca8bf73
Adam Champagne <adamchampagne@gmail.com>
2017-10-22 10:54:49 -0400

    routes working

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+
+
+var friendsArray = [
+    {
+      name: "Adam Champagne",
+      photo: "https://scontent.fewr1-1.fna.fbcdn.net/v/t1.0-9/16649278_10210268988098671_5272298271687077782_n.jpg?oh=1abaa07be1b89f260e7f66f4d35947f4&oe=5A81EAFC",
+      totals: [1,2,3,4,5,1,2,3,4,5]
+    }
+  ];
+
+  module.exports = friendsArray;
