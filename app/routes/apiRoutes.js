85c83d7fee3897292728c9fa1c63be1de1a3ca35
Adam Champagne <adamchampagne@Adams-iMac.fios-router.home>
2017-10-25 14:51:33 -0400

    switched some logic

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -18,9 +18,29 @@ module.exports = function(app) {
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
   app.post("/api/friends", function(req, res) {
-        
+
+      var userTotal = 0;
+
       friends.push(req.body);
-  
+
+      var newFriend = req.body;
+      console.log("newFriend: " + newFriend);
+      var newFriendTotals = newFriend.totals;
+      console.log("newFriendTotals: " + newFriendTotals);
+      var difference = 0;
+      var differences = [];
+
+      for (var i = 0; i < friends.length; i++) {
+          console.log("Name: " + friends[i].name);
+
+          for (var j = 0; j < newFriendTotals.length; j++) {
+            difference = Math.abs(friends[i].totals[j] - newFriend.totals[j]);
+            console.log("friends[i].totals[j]: " + friends[i].totals[j]);
+            console.log("newFriend.totals[j]): " + newFriend.totals[j]);
+            console.log("difference: " + difference);
+          }
+      }  
+
     });
 
 }
