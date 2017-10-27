4f28422c26e1b5fdbd314d2534196c1f2bb74a02
Adam Champagne <adamchampagne@Adams-iMac.fios-router.home>
2017-10-27 08:10:12 -0400

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,18 +2,18 @@
 
 var friendsArray = [
     {
-      name: "Adam Champagne",
-      photo: "https://scontent.fewr1-1.fna.fbcdn.net/v/t1.0-9/16649278_10210268988098671_5272298271687077782_n.jpg?oh=1abaa07be1b89f260e7f66f4d35947f4&oe=5A81EAFC",
+      name: "Emily Smith",
+      photo: "http://demo.templateocean.com/wrapbootstrap/stamp/image-bg/1-home-style-one/images/team/4@2x.jpg",
       scores: [1,5,3,4,5,2,2,4,6,5]
     },
     {
-      name: "Thomas Champagne",
-      photo: "https://scontent.fewr1-1.fna.fbcdn.net/v/t1.0-9/16649278_10210268988098671_5272298271687077782_n.jpg?oh=1abaa07be1b89f260e7f66f4d35947f4&oe=5A81EAFC",
+      name: "George Walker",
+      photo: "http://demo.templateocean.com/wrapbootstrap/stamp/image-bg/1-home-style-one/images/team/1@2x.jpg",
       scores: [1,6,2,4,6,2,3,4,6,5]
     },
     {
-      name: "Nicholas Champagne",
-      photo: "https://scontent.fewr1-1.fna.fbcdn.net/v/t1.0-9/16649278_10210268988098671_5272298271687077782_n.jpg?oh=1abaa07be1b89f260e7f66f4d35947f4&oe=5A81EAFC",
+      name: "Frank Hanson",
+      photo: "http://demo.templateocean.com/wrapbootstrap/stamp/image-bg/1-home-style-one/images/team/2@2x.jpg",
       scores: [1,4,3,4,5,5,5,1,6,5]
     }
     
